cf2f51bbb06c794cf13780216e1ab978df8376b6
Krzysztof Sobczak <krzysztof.sobczak@mybaze.com>
2015-04-18 22:45:48 +0200

    Initial project

---
+++ HumanConnect4/HumanConnect4.Shared/NeuralNetwork/Edge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanConnect4
+{
+    class Edge
+    {
+
+    }
+}
---
+++ HumanConnect4/HumanConnect4.Shared/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanConnect4
+{
+    class NeuralNetwork
+    {
+    }
+}
