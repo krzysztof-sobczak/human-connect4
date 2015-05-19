4c88d43ddb859e4c85714aac5c9d77fa6a6c55b5
Krzysztof Sobczak <krzysztof.sobczak@mybaze.com>
2015-05-19 21:37:45 +0200

    missing files

---
+++ HumanConnect4/HumanConnect4.Shared/Connect4/TestSets/VelenaCsvSeries.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using HumanConnect4.NeuralNetwork.Layers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace HumanConnect4.Connect4.TrainingSets
+{
+    public class VelenaCsvSeries : AbstractTrainingSet
+    {
+        public VelenaCsvSeries()
+        {
+            InputLayers = new List<InputLayer>();
+            OutputLayers = new List<OutputLayer>();
+
+            AsyncHelpers.RunSync(() => getFromVelenaCsv());
+        }
+
+        private async Task getFromVelenaCsv()
+        {
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(Windows.ApplicationModel.Package.Current.InstalledLocation.Path + @"\Resources\learn");
+            List<StorageFile> files = new List<StorageFile>(await folder.GetFilesAsync()).GetRange(0,4);
+            foreach (StorageFile file in files)
+            {
+                using (Stream stream = (await file.OpenReadAsync()).AsStreamForRead())
+                {
+                    using (var reader = new CsvReader(new StreamReader(stream)))
+                    {
+                        reader.Configuration.RegisterClassMap<TrainingSetCsvMap>();
+                        while (reader.Read())
+                        {
+                            TrainingSetCsv instance = reader.GetRecord<TrainingSetCsv>();
+                            InputLayers.Add(instance.getInputLayer());
+                            OutputLayers.Add(columnNumberToOutputLayer(instance.BestColumn));
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
---
+++ HumanConnect4/HumanConnect4.Shared/Connect4/TrainingSets/VelenaCsvSeries.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using HumanConnect4.Connect4.TrainingSets;
+using HumanConnect4.NeuralNetwork.Layers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace HumanConnect4.Connect4.TestSets
+{
+    public class VelenaCsvSeries : AbstractTestSet
+    {
+
+        public VelenaCsvSeries()
+        {
+            InputLayers = new List<InputLayer>();
+            OutputLayers = new List<OutputLayer>();
+
+            AsyncHelpers.RunSync(() => getFromVelenaCsv());
+        }
+
+        private async Task getFromVelenaCsv()
+        {
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(Windows.ApplicationModel.Package.Current.InstalledLocation.Path + @"\Resources\test");
+            List<StorageFile> files = new List<StorageFile>(await folder.GetFilesAsync());
+            foreach (StorageFile file in files)
+            {
+                using (Stream stream = (await file.OpenReadAsync()).AsStreamForRead())
+                {
+                    using (var reader = new CsvReader(new StreamReader(stream)))
+                    {
+                        reader.Configuration.RegisterClassMap<TrainingSetCsvMap>();
+                        while (reader.Read())
+                        {
+                            TrainingSetCsv instance = reader.GetRecord<TrainingSetCsv>();
+                            InputLayers.Add(instance.getInputLayer());
+                            OutputLayers.Add(columnNumberToOutputLayer(instance.BestColumn));
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
