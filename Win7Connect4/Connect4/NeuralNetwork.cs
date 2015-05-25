7a4414ed04705ff274d851a49d71d4f3721aa4e5
Krzysztof Sobczak <krzysztof.sobczak@mybaze.com>
2015-05-25 21:57:56 +0200

    enable again one network approach restored one network approach in AI

--- Win7Connect4/Connect4/NeuralNetwork.cs
+++ Win7Connect4/Connect4/NeuralNetwork.cs
@@ -28,6 +28,13 @@ namespace HumanConnect4.Connect4
             this.OutputLayer = getOutputLayer(networksLayer);
         }
 
+        public NeuralNetwork(Network network)
+        {
+            this.InputLayer = network.InputLayer;
+            this.HiddenLayers = network.HiddenLayers;
+            this.OutputLayer = network.OutputLayer;
+        }
+
         public NeuralNetwork(Network networkFrom1To6, Network networkFrom7To12, Network networkFrom13To18, Network networkFrom19To24)
         {
             this.InputLayer = getInputLayer();
@@ -37,7 +44,7 @@ namespace HumanConnect4.Connect4
             this.OutputLayer = getOutputLayer(networksLayer);
         }
 
-        public static NeuralNetwork fromXml(string filepath)
+        public static NeuralNetwork getNeuralNetworkfromXml(string filepath)
         {
             System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(NeuralNetwork), new Type[] { typeof(Network), typeof(OutputLayer), typeof(InputLayer), typeof(PassiveNeuron), typeof(Neuron), typeof(Edge), typeof(Layer), typeof(ConvolutionLayer) });
             var streamRead = new FileStream(filepath, FileMode.Open);
@@ -166,7 +173,7 @@ namespace HumanConnect4.Connect4
             return outputLayer;
         }
 
-        override public void saveToXml(string filepath)
+        public void saveNeuralNetworkToXml(string filepath)
         {
             System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(NeuralNetwork), new Type[] { typeof(Network), typeof(OutputLayer), typeof(InputLayer), typeof(PassiveNeuron), typeof(Neuron), typeof(Edge), typeof(Layer), typeof(ConvolutionLayer) });
             var streamWrite = new FileStream(filepath, FileMode.Create);
