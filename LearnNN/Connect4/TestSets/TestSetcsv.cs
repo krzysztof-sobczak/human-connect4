6aac46ba597c900aaaee281bf273f86ad035916d
Dawid Pachowski <dawidppp@tlen.pl>
2015-06-03 17:56:59 +0200

    Added: test maping for few good moves.

---
+++ LearnNN/Connect4/TestSets/TestSetcsv.cs
@@ -0,0 +1,40 @@
+﻿using HumanConnect4.NeuralNetwork.Layers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanConnect4.Connect4.TrainingSets
+{
+    public class TestSetCsv
+    {
+        public List<string> Contexts { get; set; }
+        public List<int> BestColumns { get; set; }
+
+        public InputLayer getInputLayer()
+        {
+            InputLayer inputLayer = new InputLayer();
+
+            foreach(string context in Contexts)
+            {
+                ExtendedContext extendedContext = getExtendedContext(context);
+                inputLayer.Neurons.AddRange(extendedContext.getPassiveNeurons());
+            }
+
+            return inputLayer;
+        }
+
+        private ExtendedContext getExtendedContext(string context)
+        {
+            var splittedContext = context.Replace('{',' ').Replace('}',' ').Split(',');
+            List<int> contextValues = new List<int>();
+            foreach (string stringValue in splittedContext)
+            {
+                int value = 0;
+                int.TryParse(stringValue, out value);
+                contextValues.Add(value);
+            }
+            return new ExtendedContext(contextValues);
+        }
+    }
+
+}
